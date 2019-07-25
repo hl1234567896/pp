272d1c749072f6c2b6b7357f0642df27118be0a5
hl <850910086@qq.com>
2019-07-25 20:21:06 +0800

    添加了log

--- 1231.py
+++ 1231.py
@@ -5,3 +5,9 @@ from django.http import  HttpResponse
 def index(requests):
     print('卢本伟牛逼')
     return HttpResponse('我卢本伟,没有开挂')
+
+
+def log(request):
+    print('这是登录视图')
+
+    return HttpResponse()
